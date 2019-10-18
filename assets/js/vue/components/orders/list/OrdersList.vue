036059033385e1f0a6753d032d25db103b34eadb
Roman Kowalski <roman.kowalski@dealercrm.pl>
2019-10-18 23:45:03 +0200

    sprzątanie listy zamówień

--- assets/js/vue/components/orders/list/OrdersList.vue
+++ assets/js/vue/components/orders/list/OrdersList.vue
@@ -10,14 +10,6 @@
             </div>
         </filters>
 
-        <production-plan 
-            :showModal="showProductionPlan" 
-            :departments="departments"
-            :production="selectedProduction"
-            :orderContext="selectedOrder"
-            @closeModal="closePlan($event)" 
-        />
-
         <br>
 
         <table-plus :headers="tableHeaders" :loading="loading" :initialSort="'l.confirmedDate'" @sortChanged="updateSort">
@@ -25,7 +17,7 @@
                 <td>{{ agreement.header.orderNumber || agreement.line.id }}</td>
                 <td>{{ agreement.header.createDate }}</td>
                 <td>{{ agreement.line.confirmedDate }}</td>
-                <td>{{ customerName(agreement.customer) }}</td>
+                <td>{{ __mixin_customerName(agreement.customer) }}</td>
                 <td>{{ agreement.product.name }}
                     <tooltip v-if="agreement.line.description.length > 0">
                         <i slot="visible-content" class="fa fa-info-circle hasTooltip"></i>
@@ -38,29 +30,6 @@
                         {{ getProductionStatusData(agreement.production)['title'] }}
                     </span>
                 </td>
-                <td>
-                    <dropdown class="icon-only">
-                        <a class="dropdown-item" :href="getRouting().get('agreement_line_details') + '/' + agreement.line.id">
-                            <i class="fa fa-tasks" aria-hidden="true"></i> Panel
-                        </a>
-
-                        <a class="dropdown-item" :href="getRouting().get('orders_edit') + '/' + agreement.header.id">
-                            <i class="fa fa-pencil" aria-hidden="true"></i> Edycja
-                        </a>
-
-                        <a class="dropdown-item" href="#"
-                           v-if="agreement.production && agreement.production.data.length === 0 && canUserStartProduction()"
-                           @click="handleRunProduction(agreement)"
-                        >
-                            <i class="fa fa-cogs" aria-hidden="true"></i> Przekaż na produkcję
-                        </a>
-
-                        <a class="dropdown-item text-danger" href="#" @click.prevent="confirmDeleteModal(agreement)" v-if="canUserDeleteOrder()">
-                            <i class="fa fa-trash text-danger" aria-hidden="true"></i> Usuń
-                        </a>
-
-                    </dropdown>
-                </td>
 
                 <td>
                     <line-actions :line="agreement" @lineChanged="fetchData()"></line-actions>
@@ -68,27 +37,6 @@
 
             </tr>
         </table-plus>
-
-        <confirmation-modal
-            :show="confirmations.delete.show"
-            :busy="confirmations.delete.busy"
-            @answerYes="handleDelete(confirmations.delete.context)"
-            @closeModal="confirmations.delete.show = false"
-            v-if="confirmations.delete.show"
-        >
-            <div>
-                <p><strong>Czy na pewno usunąć zlecenie numer '{{ confirmations.delete.context.header.orderNumber }}'?</strong></p>
-
-                <ul class="list-unstyled">
-                    <li v-for="product in getOrderedProducts(confirmations.delete.context)">{{ product.name }}</li>
-                </ul>
-                <div class="alert alert-danger" v-if="hasProduction(confirmations.delete.context)">
-                    <i class="fa fa-exclamation-circle" aria-hidden="true"></i>
-                    Usunięcie zamówienia usunie również wszystkie związane z nim działania na produkcji.
-                </div>
-            </div>
-
-        </confirmation-modal>
 
     </div>
 </template>
@@ -98,7 +46,6 @@
     import moment from 'moment';
     import Filters from './Filters';
     import Dropdown from '../../base/Dropdown';
-    import ProductionPlan from './production-plan';
     import api from '../../../api/neworder';
     import routing from  '../../../api/routing';
     import ConfirmationModal from "../../base/ConfirmationModal";
@@ -109,7 +56,7 @@
     export default {
         name: "OrdersList",
 
-        components: { Filters, ProductionPlan, Dropdown, ConfirmationModal, TablePlus, Tooltip, LineActions },
+        components: { Filters, Dropdown, ConfirmationModal, TablePlus, Tooltip, LineActions },
 
         props: {
             statuses: {
@@ -194,26 +141,6 @@
 
         methods: {
 
-            handleDelete(order) {
-                this.confirmations.delete.busy = true;
-
-                api.deleteOrder(order.header.id)
-                    .then(() => {
-                        this.agreements = this.agreements.filter(agreement => {
-                            return agreement.header.id !== order.header.id;
-                        });
-                    })
-                    .finally(() => {
-                        this.confirmations.delete.busy = false;
-                        this.confirmations.delete.show = false;
-                    });
-            },
-
-            confirmDeleteModal(item) {
-                this.confirmations.delete.context = item;
-                this.confirmations.delete.show = true;
-            },
-
             setFilters() {
                 let query = qs.parse(window.location.search, { ignoreQueryPrefix: true });
                 this.filters.dateStart.start = query.dateStart || '';
@@ -228,23 +155,6 @@
                 if ((!this.filters.dateStart.end || !moment(this.filters.dateStart.end).isValid())) {
                     this.filters.dateStart.end = moment().endOf('month').format('YYYY-MM-DD');
                 }
-            },
-
-            beforeShowPlan(key) {
-                this.selectedOrder = this.agreements[key].line.id;
-                this.selectedProduction = this.agreements[key].production.data;
-                this.showProductionPlan = true;
-            },
-
-            closePlan(event) {
-                this.showProductionPlan = false;
-                let order = this.agreements.find(item => { return item.line.id === this.selectedOrder; });
-
-                if (event) {
-
-                    order.production.data = event[0];    
-                }
-                
             },
 
             handleFiltersChange(date) {
@@ -268,97 +178,6 @@
                     })
                     .catch(data => {})
                     .finally(() => { this.loading = false; });
-            },
-
-            customerName(customer) {
-
-                if (!customer) {
-                    return '';
-                }
-                let result = customer.name;
-
-                if (customer.first_name || customer.last_name) { 
-                    result = result + ' (' + [customer.first_name, customer.last_name].join(' ') + ')';
-                }
-                
-                return result;
-            },
-
-            prodBtnDescription(key) {
-                if (!this.agreements[key]) {
-                    return '';
-                };
-
-                if (this.agreements[key].production.data.length === 0) {
-                    return 'Zleć';
-                }
-                else {
-                    return 'Edytuj';
-                }
-            },
-
-            prodBtnIcon(key) {
-                if (!this.agreements[key]) {
-                    return '';
-                };
-
-                if (this.agreements[key].production.data.length === 0) {
-                    return 'fa fa-list-ol';
-                }
-                else {
-                    return 'fa fa-pencil';
-                }
-            },
-
-            handleRunProduction(agreement) {
-
-                let production = this.departments.map(department => {
-                    return {
-                        slug: department.slug,
-                        name: department.name,
-                        status: 0,
-                        dateFrom: null,
-                        dateTo: null
-                    };
-                });
-
-
-                api.storeProductionPlan(production, agreement.line.id)
-                    .then(({data}) => {
-                        agreement.production.data = Array.isArray(data) ? data[0] : [];
-                        Event.$emit('message', {
-                            type: 'success',
-                            content: 'Dodano do harmonogramu produkcji.'
-                        });
-                        this.fetchData();
-                        Event.$emit('statusUpdated');
-                    })
-                    .finally(() => {
-
-                    })
-            },
-
-            hasProduction(agreement) {
-                for (let testing of this.agreements) {
-                    if (testing.header.id === agreement.header.id) {
-                        if (testing.production.data.length) {
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            },
-
-            getOrderedProducts(agreement) {
-                let result = [];
-
-                for (let testing of this.agreements) {
-                    if (testing.header.id === agreement.header.id) {
-                        result.push(testing.product);
-                    }
-                }
-
-                return result;
             },
 
             updateSort(event) {
@@ -419,29 +238,6 @@
 
             },
 
-            getProductionStatusClass(production) {
-                if (!production) {
-                    return '';
-                }
-                if (production.data.length === 0) {
-                    return 'badge-danger';
-                }
-            },
-
-            getRouting() {
-                return routing;
-            },
-
-            canUserStartProduction() {
-                return this.$user.can(this.$privilages.CAN_PRODUCTION);
-            },
-
-            canUserDeleteOrder() {
-                return this.$user.can(this.$privilages.CAN_ORDERS_DELETE);
-            },
-
-
-
         },
         computed: {
             tableHeaders() {
@@ -455,7 +251,6 @@
                         { name: 'Status zamówienia' },
                         { name: 'Status produkcji' },
                         { name: 'Akcje' },
-                        { name: 'Akcje New' },
                     ]
                 ];
 
