865739704634fa9595cc371143b8851ed1feb72d
Roman Kowalski <roman@erla.pl>
2024-12-09 20:37:37 +0000

    usuniecie doctrine-cache-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -16,7 +16,6 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
