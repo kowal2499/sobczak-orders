2dcfd56f32e60cb4f17c6ca0d7c7f598b46c6768
Roman Kowalski <roman.kowalski@grupadealer.pl>
2025-06-29 21:40:42 +0200

    wip

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
 ];
