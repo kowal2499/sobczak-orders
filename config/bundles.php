d435d1b7953dd68a6456d6d060bbe9492a3f502c
Roman Kowalski <kowal2499@gmail.com>
2025-07-11 22:40:56 +0200

    front - dalsze sprzątanie

--- config/bundles.php
+++ config/bundles.php
@@ -19,5 +19,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
-    Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
 ];
